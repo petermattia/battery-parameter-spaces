655893d80d928647faa78421ff336d09de092823
petermattia <pattia@stanford.edu>
2018-02-20 17:14:11 -0800

    Initial commit

---
+++ contour_perimprovement.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 07:29:14 2018
+
+@author: peter
+"""
+
+from thermalsim import thermalsim
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.close('all')
+
+LOWER_CRATE_LIM = 1.6 # C rate
+UPPER_CRATE_LIM = 8   # C rate
+chargetime = 10       # [=] minutes
+
+# Import policies
+policies = np.genfromtxt('policies.csv', delimiter=',')
+
+margin = 0.8 # plotting margin
+
+# Calculate Q1(CC1, CC2) values for contour lines
+CC1 = np.arange(LOWER_CRATE_LIM-margin,UPPER_CRATE_LIM + margin,0.01)
+CC2 = np.arange(LOWER_CRATE_LIM-margin,UPPER_CRATE_LIM + margin,0.01)
+[X,Y] = np.meshgrid(CC1,CC2)
+Q1 = (100)*(chargetime - ((60*0.8)/Y))/((60/X)-(60/Y))
+Q1[np.where(Q1<0)]  = float('NaN')
+Q1[np.where(Q1>80)] = float('NaN')
+Q1_values = np.arange(5,76,10)
+
+## Create contour plot
+## Initialize plot 1: color = SOC1
+plt.figure() # x = CC1, y = CC2, contours = Q1
+plt.set_cmap('winter_r')
+C = plt.contour(X,Y,Q1,colors='k',zorder=1)
+plt.clabel(C, fontsize=10,fmt='%1.0f')
+plt.title('Time to 80% = ' + str(chargetime) + ' minutes',fontsize=16)
+plt.xlabel('CC1',fontsize=16)
+plt.ylabel('CC2',fontsize=16)
+plt.axis('square')
+plt.xlim((LOWER_CRATE_LIM-margin, UPPER_CRATE_LIM+margin))
+plt.ylim((LOWER_CRATE_LIM-margin, UPPER_CRATE_LIM+margin))
+
+# Make full screen
+manager = plt.get_current_fig_manager()
+manager.window.showMaximized()
+
+## CALCULATE POLICY LIFETIMES
+lifetime = np.zeros((len(policies),1))
+lifetimechange = np.zeros((len(policies),1))
+
+for i in range(len(policies)):
+    C1 = policies[i,0]
+    C2 = policies[i,1]
+    print('\nC1 = ' + '{:.2f}'.format(C1) + ', C2 = ' + '{:.2f}'.format(C2))
+    lifetime[i] = thermalsim(C1,C2,variance=False)
+    if i==0: baseline = np.asscalar(lifetime[0])
+    lifetimechange[i] = 100*(lifetime[i]-baseline)/baseline
+    print('Change rel. to 1-step = ' + '{:.1%}'.format(np.asscalar(lifetimechange[i])/100))
+
+## PLOT POLICIES
+plt.scatter(policies[:,0],policies[:,1],c=lifetimechange.ravel(),zorder=2)
+plt.scatter(policies[0,0],policies[0,1],marker='s',c=0,zorder=3)
+
+# Colorbar
+cbar = plt.colorbar()
+plt.clim(-100,100)
+cbar.set_label('% improvement over 1-step policy')
+
+plt.savefig('contour_lifetimechanges.png', bbox_inches='tight')
---
+++ policies.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+policies.py generates 2-step polices based 
+
+Created on Tue Feb 20 16:04:53 2018
+
+@author: peter
+"""
+
+import numpy as np
+
+# PARAMETERS TO CREATE POLICY SPACE
+LOWER_CRATE_LIM = 1.6 # C rate, lower cutoff
+UPPER_CRATE_LIM = 8   # C rate, upper cutoff
+LOWER_SOC1_LIM  = 10  # [%], lower SOC1 cutoff
+UPPER_SOC1_LIM  = 70  # [%], upper SOC1 cutoff
+DENSITY         = 10  # Points per line cut
+STEP            = 0.2 # initial distance from baseline policy
+chargetime      = 10  # [=] minutes
+
+# C1 > C2
+C1grida = np.linspace(4.8 + STEP,UPPER_CRATE_LIM,DENSITY)
+C2grida = np.linspace(LOWER_CRATE_LIM,4.8 - STEP,DENSITY)
+X2a, Y2a = np.meshgrid(C1grida, C2grida)
+
+# Remove bad policies
+for i in np.arange(0,len(X2a)):
+    for j in np.arange(0,len(Y2a)):
+        C1 = X2a[i,j]
+        C2 = Y2a[i,j]
+        SOC1 = 100 * ( chargetime - (60*0.8/C2) ) / (60/C1 - 60/C2)
+        # removes policies that are basically 1-step
+        if SOC1 < LOWER_SOC1_LIM or SOC1 > UPPER_SOC1_LIM: 
+            X2a[i,j] = float('NaN')
+            Y2a[i,j] = float('NaN')
+
+# C1 < C2
+C1gridb = np.linspace(LOWER_CRATE_LIM,4.8 - STEP,DENSITY)
+C2gridb = np.linspace(4.8 + STEP,UPPER_CRATE_LIM,DENSITY)
+X2b, Y2b = np.meshgrid(C1gridb, C2gridb)
+
+# Remove bad policies
+for i in np.arange(0,len(X2b)):
+    for j in np.arange(0,len(Y2b)):
+        C1 = X2b[i,j]
+        C2 = Y2b[i,j]
+        SOC1 = 100 * ( chargetime - (60*0.8/C2) ) / (60/C1 - 60/C2)
+        # removes policies that are basically 1-step
+        if SOC1 < LOWER_SOC1_LIM or SOC1 > UPPER_SOC1_LIM:
+            X2b[i,j] = float('NaN')
+            Y2b[i,j] = float('NaN')
+
+## Unravel, merge, clean, and add baseline policy
+X2 = np.concatenate((X2a.ravel(), X2b.ravel()))
+Y2 = np.concatenate((Y2a.ravel(), Y2b.ravel()))
+X2 = X2[~np.isnan(X2)]
+Y2 = Y2[~np.isnan(Y2)]
+X2 = np.insert(X2, 0, 4.8)
+Y2 = np.insert(Y2, 0, 4.8)
+
+print(str(len(X2)) + " total policies")
+
+# Save policies
+np.savetxt('policies.csv',np.c_[X2,Y2],delimiter=',', fmt='%1.3f')
