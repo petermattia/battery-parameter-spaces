30b3ed0f77264423ae43cbab92bc8f7f9d589ff1
petermattia <pattia@stanford.edu>
2018-06-29 22:52:24 -0700

    Sim=lo results with sigma=102

--- Simulation parameter spaces/analyze_log.py
+++ Simulation parameter spaces/analyze_log.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 
-sims = pd.read_csv('log.csv')
+sims = pd.read_csv('log2.csv')
 
 for sim_name, sim_group in sims.groupby('sim mode'):
     print('Starting sim mode:', sim_name)
