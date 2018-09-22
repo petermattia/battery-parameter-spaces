8b2fda5219e7a07b1a32de1ca41ea55e80de4c9d
petermattia <pattia@stanford.edu>
2018-09-22 12:07:28 -0700

    Leaderboard plot

--- data/leaderboardplot.py
+++ data/leaderboardplot.py
@@ -41,34 +41,28 @@ fig = plt.figure()
 plt.style.use('classic')
 manager = plt.get_current_fig_manager() # Make full screen
 manager.window.showMaximized()
-
-
-# FUNCTION FOR LOOPING THROUGH BATCHES
-def make_frame(k2):
-    plt.cla()
     
-    ## PLOT POLICIES
-    plt.ylim((0,24))
-    plt.xlim((0,10))
-    plt.axis('off')
-    
-    plt.text(5,22,'Before batch ' + str(k2+1),fontsize=24,horizontalalignment='center')
-    
-    best_indices = np.flip(np.argsort(means[k2]),axis=0)
-    
-    for k3 in np.arange(10):
-        k = best_indices[k3]
-        plt.text(5,20-k3*2,str(k3+1) + ': Policy ' + str(k) + ' '
-                 + str(param_space[k]) + 
-                 ', ' + str(int(means[k2][k])) + 
-                 ' (' + str(int(lbs[k2][k])) + ',' + str(int(ubs[k2][k])) + ')',
-                 fontsize=20,
-                 horizontalalignment='center',
-                 verticalalignment='center')
-    return fig
+## PLOT POLICIES
+plt.xlim((0,batchnum))
+plt.ylim((0,11))
+plt.gca().invert_yaxis()
+plt.xlabel('Batch index')
+plt.ylabel('Policy ranking (1=best)')
 
-## SAVE ANIMATION
-anim = animation.FuncAnimation(fig, make_frame, frames=batchnum,
-                               interval=1000, blit=False)
+for k in np.arange(batchnum):
+    best_indices = np.flip(np.argsort(means[k]),axis=0)
+    
+    for k2 in np.arange(10):
+        k3 = best_indices[k2]
+        ranking_k = np.zeros(batchnum)
+        for k4 in np.arange(batchnum):
+            best_indices_2 = np.flip(np.argsort(means[k4]),axis=0)
+            ranking_k[k4] = np.where(best_indices_2 == k3)[0][0]
+        
+        plt.plot(np.arange(batchnum),ranking_k+1)
+        
+        if k==batchnum-1:
+            plt.text(4,k2+1,str(k3) + ': ' + str(param_space[k3]),
+                     fontsize=16,verticalalignment='center')
 
-anim.save('plots/leaderboard_animate.gif', writer='imagemagick', fps=0.5)
+plt.savefig('plots/leaderboardplot.png',bbox_inches='tight')
