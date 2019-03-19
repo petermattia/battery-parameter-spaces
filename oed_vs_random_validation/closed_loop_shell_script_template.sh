e9b86d4be812681b6474448b4ad79580858b91f2
petermattia <pattia@stanford.edu>
2019-03-19 13:41:37 -0700

    oed_vs_random_validation

---
+++ oed_vs_random_validation/closed_loop_shell_script_template.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo *******NEW SIMULATION******
+# Set hyperparamaters
+beta=1
+gamma=1
+epsilon=0.8
+seed=0
+
+# Display hyperparameters
+echo beta=$(echo "$beta"|bc) gamma=$(echo "$gamma"|bc) epsilon=$(echo "$epsilon"|bc) seed=$seed
+
+# Loop through each round
+for i in 0 1 2 3 4 5 6 7
+do
+    python closed_loop_oed.py --round_idx=$i --init_beta=$beta --gamma=$gamma --epsilon=$epsilon --seed=$seed
+    python generate_predictions.py --round_idx=$i --seed=$seed
+done
+
+# One more round
+python closed_loop_oed.py --round_idx=8 --init_beta=$beta --gamma=$gamma --epsilon=$epsilon --seed=$seed
